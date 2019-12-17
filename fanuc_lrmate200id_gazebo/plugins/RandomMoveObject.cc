14140ed2b9a89750952dd6cfd2c2832fc336b566
jediofgever <fetulahatas1@gmail.com>
2019-12-17 16:55:39 +0800

    math3 to math4  in gazebo

--- fanuc_lrmate200id_gazebo/plugins/RandomMoveObject.cc
+++ fanuc_lrmate200id_gazebo/plugins/RandomMoveObject.cc
@@ -1,23 +1,19 @@
+#include <ros/ros.h>
 #include <functional>
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <ignition/math3/ignition/math/Vector3.hh>
-#include <ros/ros.h>
+#include <ignition/math4/ignition/math/Vector3.hh>
 
-double fRand(double fMin, double fMax)
-{
+double fRand(double fMin, double fMax) {
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
 }
 
-namespace gazebo
-{
-class RandomModelMove : public ModelPlugin
-{
-public:
-    void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
-    {
+namespace gazebo {
+class RandomModelMove : public ModelPlugin {
+   public:
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
         // Store the pointer to the model
         this->model = _parent;
         world_ = this->model->GetWorld();
@@ -31,42 +27,41 @@ public:
     }
 
     // Called by the world update start event
-public:
-    void OnUpdate()
-    {
+   public:
+    void OnUpdate() {
         // Apply a small linear velocity to the modeltNsm=
 
-        if ((world_->RealTime() - previous_time) > 2.0)
-        {
-
+        if ((world_->RealTime() - previous_time) > 2.0) {
             this->model->SetGravityMode(false);
             ignition::math::Pose3d current_pose = this->model->WorldPose();
             ignition::math::Vector3d current_pos = current_pose.Pos();
             ignition::math::Quaterniond current_rot(0, 0, 0, 1);
-            current_pos = ignition::math::Vector3d(fRand(1.075, 1.525), fRand(-2.85, -2.15), fRand(1.1, 1.35));
+            current_pos = ignition::math::Vector3d(
+                fRand(1.075, 1.525), fRand(-2.85, -2.15), fRand(1.1, 1.35));
             ignition::math::Pose3d random_pose(current_pos, current_rot);
-            //this->model->SetWorldPose(random_pose);
-            //this->model->SetAngularVel(ignition::math::Vector3d(0.0, 0.0, 0.0));
+            // this->model->SetWorldPose(random_pose);
+            // this->model->SetAngularVel(ignition::math::Vector3d(0.0, 0.0,
+            // 0.0));
             previous_time = world_->RealTime();
         }
 
-        //sleep(1.0);
+        // sleep(1.0);
 
-        //current_pos[2] = 1.1;
-        //current_pose.Set(current_pos, current_rot);
+        // current_pos[2] = 1.1;
+        // current_pose.Set(current_pos, current_rot);
     }
 
     // Pointer to the model
-private:
+   private:
     physics::ModelPtr model;
     gazebo::physics::WorldPtr world_;
     common::Time previous_time;
 
     // Pointer to the update event connection
-private:
+   private:
     event::ConnectionPtr updateConnection;
 };
 
 // Register this plugin with the simulator
 GZ_REGISTER_MODEL_PLUGIN(RandomModelMove)
-} // namespace gazebo
+}  // namespace gazebo
--- fanuc_lrmate200id_gazebo/plugins/SimulationPickandPlace.hh
+++ fanuc_lrmate200id_gazebo/plugins/SimulationPickandPlace.hh
@@ -1,29 +1,25 @@
+#include <ros/ros.h>
+#include <Eigen/Core>
 #include <functional>
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <ignition/math3/ignition/math/Vector3.hh>
-#include <ros/ros.h>
-#include <Eigen/Core>
+#include <ignition/math4/ignition/math/Vector3.hh>
 
-#include <geometry_msgs/Pose.h>
-#include <moveit/move_group_interface/move_group_interface.h>
 #include <fanuc_arm_for_chicony/pickplace/PickandPlacer.h>
-#include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
+#include <moveit/move_group_interface/move_group_interface.h>
 
 #include <fanuc_arm_for_chicony/utils.h>
 #include <jsk_recognition_msgs/BoundingBox.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 
-namespace gazebo
-{
+namespace gazebo {
 
-class SimulationPickandPlace : public WorldPlugin
-{
-
+class SimulationPickandPlace : public WorldPlugin {
     // Pointer to the model
-private:
+   private:
     physics::WorldPtr world;
 
     // Pointer to the update event connection
@@ -38,7 +34,7 @@ private:
     ros::Publisher gtBBX_pub_;
     tf::TransformListener *listener_;
 
-public:
+   public:
     SimulationPickandPlace();
     ~SimulationPickandPlace();
     void Load(physics::WorldPtr _parent, sdf::ElementPtr /*_sdf*/);
@@ -47,4 +43,4 @@ public:
 
 // Register this plugin with the simulator
 GZ_REGISTER_WORLD_PLUGIN(SimulationPickandPlace);
-} // namespace gazebo
+}  // namespace gazebo
